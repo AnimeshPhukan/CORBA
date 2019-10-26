3652621ec2fa9f42c242f06ebf10a9382a7c3e61
Animesh Phukan <46883162+AnimeshPhukan@users.noreply.github.com>
2019-10-26 09:11:25 +0530

    Add files via upload

---
+++ CORBA/CorbaAdditionServer/src/AdditionObj.java
@@ -0,0 +1,22 @@
+import org.omg.CORBA.ORB;
+
+import AdditionApp.*;
+ 
+class AdditionObj extends AdditionPOA {
+  private ORB orb;
+ 
+  public void setORB(ORB orb_val) {
+    orb = orb_val; 
+  }
+ 
+  // implement add() method
+  public int add(int a, int b) {
+    int r=a+b;
+    return r;
+  }
+ 
+  // implement shutdown() method
+  public void shutdown() {
+    orb.shutdown(false);
+  }
+}
